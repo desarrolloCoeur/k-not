704666b55a7b0a5b62b273879b4644769ce9967b
Javier André Hernández Hernández <andrehdz@MacBook-Pro-de-Javier.local>
2023-05-15 12:00:55 -0600

    modificacion de mensajes

--- app/page.js
+++ app/page.js
@@ -11,15 +11,15 @@ export default function Home() {
             <div className="flex flex-wrap md:flex-nowrap justify-center items-center gap-3 my-10 container mx-auto">
 
                 <div className=" w-10/12 h-[300px] md:w-1/3 bg-cover bg-[url(/img/fishing-home.jpeg)] flex justify-center items-end pb-5 bg-center rounded-xl" >
-                    <Link href="#" className="btn-primary">GO FISHING!</Link>
+                    <Link href="#" className="btn-primary">IR A PESCAR!</Link>
                 </div>
 
                 <div className=" w-10/12 h-[300px] md:w-1/3 bg-cover bg-[url(/img/jet-ski-home.jpg)] flex justify-center items-end pb-5 bg-center rounded-xl">
-                    <Link href="#" className="btn-primary">GO JETSKI!</Link>
+                    <Link href="#" className="btn-primary">RENTAR JETSKIS!</Link>
                 </div>
 
                 <div className=" w-10/12 h-[300px] md:w-1/3 bg-cover bg-[url(/img/tours-home.jpg)] flex justify-center items-end pb-5 bg-center rounded-xl">
-                    <Link href="#" className="btn-primary">GO TOURS!</Link>
+                    <Link href="#" className="btn-primary">VER TOURS!</Link>
                 </div>
             </div>
 
