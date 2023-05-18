0f83a2c70b11caf50e40cbbe419b0cdc37a605a7
CoeurMkt <desarrollo@coeurmkt.com>
2023-05-18 11:47:22 -0600

    cambios

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -2,10 +2,11 @@ import Link from "next/link"
 
 const Hero = () => {
   return (
-    <div className=" bg-cover bg-[url(/img/kayak-slider.jpeg)] relative">
+    <div className=" bg-cover bg-[url(/img/kayak-slider.jpeg)] bg-center relative">
         <div className="container w-11/12 relative min-h-[100dvh] mx-auto"> 
-          <div className=" absolute bottom-10 flex gap-5">
-              <Link href="#" className="btn-primary">RESERVE NOW</Link>
+          <div className=" absolute bottom-10 flex flex-col gap-1">
+              <h2 className="md:text-7xl font-bold shadow-md bg-slate-50 py-1 px-2 rounded-md">kayak en el Oceano</h2>
+              <Link href="#" className="btn-primary w-[170px] text-xl text-center ">RESERVE NOW</Link>
           </div>
         </div>
     </div>
